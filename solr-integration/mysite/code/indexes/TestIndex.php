4de60dbc4ebd88a7461acb29a5d21bffc761a2a1
Juan van den Anker <juan.anker@dia.govt.nz>
2017-02-21 09:32:50 +1300

    Index file contents and file specified by path contents

--- solr-integration/mysite/code/indexes/TestIndex.php
+++ solr-integration/mysite/code/indexes/TestIndex.php
@@ -3,14 +3,19 @@
 class TestIndex extends SolrIndex {
 
     function init() {
-        //$this->addClass('SiteTree');
-        //$this->addClass('StaffMember');
-        //$this->addClass('GalleryPage');
+    	// SimpleObject contains a field with a path to a file
+        $this->addClass('SimpleObject');
+
+        // Index all File objects
 		$this->addClass('File');
 
         //$this->addAllFulltextFields();
+
+		// FileTextExtractable adds a field FileContent to each File object
 		$this->addFulltextField('FileContent');
-		//$this->addStoredField('FileContent', 'HTMLText');
+
+		// Has to be in here according to the documentation
+		$this->addFilterField('ShowInSearch');
     }
 
 }
--- solr-integration/mysite/code/tasks/TestTask.php
+++ solr-integration/mysite/code/tasks/TestTask.php
@@ -4,21 +4,33 @@ class TestTask extends BuildTask {
 
     public function run($request) {
 
-        $index = singleton('TestIndex');
+		DB::query("DELETE FROM File");
+		DB::query("DELETE FROM SimpleObject");
 
-        $page = new Page(array(
-            'Content' => 'Document 1.'));
+        $page = new Page(array('Content' => 'Document 1.'));
         $page->write();
 
-        $page = new SiteTree(array(
-            'Content' => 'Document 2.'));
+        $page = new SiteTree(array('Content' => 'Document 2.'));
         $page->write();
 
-        $query = new SearchQuery();
-        $query->search('My house is on fire');
+		$file = new File();
+		$file->Filename = 'assets/test.pdf';
+		$file->write();
 
-        $results = $index->search($query);
-        print_r($results);
+		$filename = 'assets/test/test.txt';
+		$fh = fopen(BASE_PATH . '/' . $filename, "w");
+		fwrite($fh, 'Of properly carriage shutters ye as wandered up repeated moreover');
+		fclose($fh);
+
+		$file = new File();
+		$file->Filename = $filename;
+		$file->ParentID = Folder::find_or_make('/test')->ID;
+		$file->write();
+
+		$o = new SimpleObject();
+		$o->FileID = $file->ID;
+		$o->FileName = BASE_PATH . '/assets/test.pdf';
+		$o->write();
     }
 
 }
